dbe4319c99b9f4b5427110fc8d9f859c946178af
shiyindaxiaojie <1813986321@qq.com>
2022-06-01 00:13:21 +0800

    变更

---
+++ eden-components/eden-spring-integration/src/main/java/org/ylzl/eden/spring/integration/bpc/process/Processor.java
@@ -0,0 +1,12 @@
+package org.ylzl.eden.spring.integration.bpc.process;
+
+/**
+ * 流程执行器
+ *
+ * @author <a href="mailto:shiyindaxiaojie@gmail.com">gyl</a>
+ * @since 2.4.x
+ */
+public interface Processor {
+
+
+}
---
+++ eden-components/eden-spring-integration/src/main/java/org/ylzl/eden/spring/integration/bpc/process/util/ProcessUtils.java
@@ -0,0 +1,12 @@
+package org.ylzl.eden.spring.integration.bpc.process.util;
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:shiyindaxiaojie@gmail.com">gyl</a>
+ * @since 2.4.x
+ */
+public class ProcessUtils {
+
+
+}
