1f833045b2a8f618274691a18753a86ae3af4306
guoyuanlu <guoyuanlu@puyiwm.com>
2022-10-08 14:00:08 +0800

    fixed

--- eden-components/eden-solutions/eden-cache-adapter/src/main/java/org/ylzl/eden/cache/adapter/hotkey/HotKey.java
+++ eden-components/eden-solutions/eden-cache-adapter/src/main/java/org/ylzl/eden/cache/adapter/hotkey/HotKey.java
@@ -1,14 +1,11 @@
 package org.ylzl.eden.cache.adapter.hotkey;
 
-import org.ylzl.eden.cache.adapter.extension.SPI;
-
 /**
  * TODO
  *
  * @author <a href="mailto:shiyindaxiaojie@gmail.com">gyl</a>
  * @since 2.4.13
  */
-@SPI
 public interface HotKey {
 
 
