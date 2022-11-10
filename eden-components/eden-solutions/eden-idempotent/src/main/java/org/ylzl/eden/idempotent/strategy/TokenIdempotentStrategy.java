373d531c076fcc314c8ea0877c3716fe54bd7caa
shiyindaxiaojie <1813986321@qq.com>
2022-11-11 00:41:47 +0800

    fixed

--- eden-components/eden-solutions/eden-idempotent/src/main/java/org/ylzl/eden/idempotent/strategy/TokenIdempotentStrategy.java
+++ eden-components/eden-solutions/eden-idempotent/src/main/java/org/ylzl/eden/idempotent/strategy/TokenIdempotentStrategy.java
@@ -1,5 +1,7 @@
 package org.ylzl.eden.idempotent.strategy;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 基于令牌策略管理幂等请求
  *
@@ -11,14 +13,16 @@ public interface TokenIdempotentStrategy {
 	/**
 	 * 生成请求令牌
 	 *
+	 * @param ttl 存活时间
+	 * @param timeUnit 时间单位
 	 * @return 请求令牌
 	 */
-	String generate();
+	String generateToken(long ttl, TimeUnit timeUnit);
 
 	/**
 	 * 校验请求令牌
 	 *
 	 * @param token 请求令牌
 	 */
-	void validate(String token);
+	void validateToken(String token);
 }
