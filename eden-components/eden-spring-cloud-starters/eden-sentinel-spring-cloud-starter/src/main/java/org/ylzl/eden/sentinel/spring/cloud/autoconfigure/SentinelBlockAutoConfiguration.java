df111a00ff314f77aa24b9c069d5ebe9748e194b
shiyindaxiaojie <1813986321@qq.com>
2022-11-19 00:40:07 +0800

    fixed

--- eden-components/eden-spring-cloud-starters/eden-sentinel-spring-cloud-starter/src/main/java/org/ylzl/eden/sentinel/spring/cloud/autoconfigure/SentinelBlockAutoConfiguration.java
+++ eden-components/eden-spring-cloud-starters/eden-sentinel-spring-cloud-starter/src/main/java/org/ylzl/eden/sentinel/spring/cloud/autoconfigure/SentinelBlockAutoConfiguration.java
@@ -3,6 +3,7 @@ package org.ylzl.eden.sentinel.spring.cloud.autoconfigure;
 import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.ylzl.eden.spring.cloud.sentinel.web.CustomBlockExceptionHandler;
@@ -14,6 +15,7 @@ import org.ylzl.eden.spring.cloud.sentinel.web.CustomBlockExceptionHandler;
  * @since 2.4.13
  */
 @ConditionalOnClass(BlockExceptionHandler.class)
+@ConditionalOnProperty(name = "spring.cloud.sentinel.enabled", matchIfMissing = true)
 @Slf4j
 @Configuration(proxyBeanMethods = false)
 public class SentinelBlockAutoConfiguration {
